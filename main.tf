a087820ee0d12de8f390992d9974c3f346d10572
mcadevops <140480388+mcadevops@users.noreply.github.com>
2023-10-03 12:30:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,51 +12,26 @@ provider "aws" {
   secret_key= "var.secretkey"
   region     = "us-east-1"
 }
-resource "aws_instance" "myec2" {
+resource "aws_instance" "ownec2" {
   ami           = "ami-03a6eaae9938c858c"
   instance_type = "t2.micro"
-  vpc_security_group_ids=[aws_security_group.web-sg.id]
+  vpc_security_group_ids=[aws_security_group.ownsg.id]
   key_name="tf-key-pair"
-
-
-
 tags={
- Name="web-server"
+ Name="terraform-plan"
 }
-user_data= <<-EOF
-#!/bin/bash
-yum install httpd -y
-service httpd start
-cd /var/www/html
-touch index.html
-echo "hello from Terraform" > index.html
-EOF
 }
-resource "aws_security_group" "web-sg" {
- name="web-sg"
+resource "aws_security_group" "ownsg" {
+ name="ownsg"
 ingress {
  from_port=80
  to_port=80
 protocol="tcp"
 cidr_blocks= ["0.0.0.0/0"]
-}
 
-
-
-ingress {
- from_port=22
- to_port=22
-protocol="tcp"
-cidr_blocks= ["0.0.0.0/0"]
-}
-egress {
- from_port=0
- to_port=0
-protocol="-1"
-cidr_blocks= ["0.0.0.0/0"]
 }
 }
-resource "aws_key_pair" "tf-key-pair-1" {
+resource "aws_key_pair" "tf-key-pair-demo" {
 key_name = "tf-key-pair"
 public_key = tls_private_key.rsa.public_key_openssh
 }
@@ -66,5 +41,5 @@ rsa_bits  = 4096
 }
 resource "local_file" "tf-key" {
 content  = tls_private_key.rsa.private_key_pem
-filename = "tf-key-pair-1"
+filename = "tf-key-pair-demo"
 }
