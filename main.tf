36101a40ca5de60b43dbe252ce0102266ccb4e83
mcadevops <140480388+mcadevops@users.noreply.github.com>
2023-10-03 11:48:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,4 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "~>4.0"
-    }
-  }
-}
+
 provider "aws" {
   access_key="var.accesskey"
   secret_key= "var.secretkey"
