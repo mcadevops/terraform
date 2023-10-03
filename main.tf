28e805606af623fa4bc3cbdf4f5e7ebd63732d5f
mcadevops <140480388+mcadevops@users.noreply.github.com>
2023-10-03 12:33:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,11 +2,9 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "5.19.0"
     }
   }
 }
-
 provider "aws" {
   access_key="var.accesskey"
   secret_key= "var.secretkey"
